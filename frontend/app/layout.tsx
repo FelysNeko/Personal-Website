4df347991b26f9dccd29a81fbd7d6dbc2654d223
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-03-30 05:08:35 -0400

    new setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const notoSans = Noto_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FelysNeko",
+  description: "Home of FelysNeko",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={notoSans.className}>
+      <Navbar />
+      <body>{children}</body>
+      <Footer />
+    </html>
+  );
+}
