a79726f232cad89560c51b106c8870eb41292dfe
FelysNeko <88437453+FelysNeko@users.noreply.github.com>
2024-05-23 00:38:58 -0400

    Update ChemCalc.tsx

--- nextjs/components/client/toolset/ChemCalc.tsx
+++ nextjs/components/client/toolset/ChemCalc.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { capitalize } from "@/utils/general";
 
 interface Props {
@@ -8,10 +8,11 @@ interface Props {
 
 const ChemCalc = ({ lang }: Props) => {
   return (
-    <div className="grid md:grid-rows-2 md:grid-flow-col gap-6">
+    <div className="grid md:grid-cols-2 md:grid-rows-6 md:grid-flow-col gap-6">
       <Atom lang={lang} />
       <Molecule lang={lang} />
       <Balance lang={lang} />
+      <Note lang={lang} />
     </div>
   );
 };
@@ -50,7 +51,7 @@ const Atom = ({ lang }: Props) => {
   };
 
   return (
-    <div>
+    <div className="md:row-span-3">
       <div className="border-b">
         <input
           className="input w-full"
@@ -129,8 +130,8 @@ const Molecule = ({ lang }: Props) => {
   };
 
   return (
-    <div>
-      <div className="border-b ">
+    <div className="md:row-span-3">
+      <div className="border-b">
         <input
           className="input w-full"
           placeholder={
@@ -172,70 +173,159 @@ const Molecule = ({ lang }: Props) => {
 };
 
 const Balance = ({ lang }: Props) => {
-  const [reCount, setReCount] = useState(2);
-  const [prCount, setPrCount] = useState(2);
+  type Equation = {
+    reactant: string[];
+    product: string[];
+  };
+
+  const [reactant, setReactant] = useState<string[]>(["", ""]);
+  const [product, setProduct] = useState<string[]>(["", ""]);
+  const [reResult, setReResult] = useState("");
+  const [prResult, setPrResult] = useState("");
+
+  const reload = async () => {
+    const response =
+      reactant.every((e) => e !== "") &&
+      product.every((e) => e !== "") &&
+      (await fetch("https://api.felysneko.com/fast/chemistry/solve", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          reactant: reactant,
+          product: product,
+        }),
+      }).catch((e) => console.log(e)));
+
+    if (response && response.ok) {
+      const data: Equation = await response.json();
+      setReResult(data.reactant.join(" + "));
+      setPrResult(data.product.join(" + "));
+    } else {
+      setReResult("");
+      setPrResult("");
+    }
+  };
+
+  useEffect(() => {
+    reload();
+  }, [reactant.length, product.length]);
 
   return (
-    <div className="lg:row-span-2">
-      <form className="grid grid-cols-2 gap-4">
+    <div className="md:row-span-4 md:relative">
+      <h1 className="text-xl font-bold">
+        {lang === "中" ? "分子式配平" : "Equation Balancer"}
+      </h1>
+      <form className="grid grid-cols-2 gap-4 mt-4" onChange={reload}>
         <div className="space-y-2">
           <ul className="border-y border-b-neutral-800 lg:border-t-neutral-800 space-y-2 py-2">
-            {Array.from({ length: reCount }).map((_, i) => (
+            {reactant.map((_, i) => (
               <li key={i}>
                 <input
-                  className="input w-full input-disabled"
+                  className="input w-full"
                   placeholder={lang === "中" ? "反应物" : "Reactant"}
+                  onChange={(e) =>
+                    setReactant((prev) => {
+                      prev[i] = e.target.value;
+                      return prev;
+                    })
+                  }
                 />
               </li>
             ))}
           </ul>
           <div className="flex gap-2 me-2">
-            <button
-              type="button"
-              className="btn btn-sm"
-              onClick={() => reCount <= 4 && setReCount(reCount + 1)}
-            >
-              {PLUS}
-            </button>
-            <button
-              type="button"
-              className="btn btn-sm"
-              onClick={() => reCount >= 2 && setReCount(reCount - 1)}
-            >
-              {DASH}
-            </button>
+            {reactant.length <= 2 && (
+              <button
+                type="button"
+                className="btn btn-sm"
+                onClick={() => setReactant((prev) => [...prev, ""])}
+              >
+                {PLUS}
+              </button>
+            )}
+            {reactant.length >= 2 && (
+              <button
+                type="button"
+                className="btn btn-sm"
+                onClick={() => setReactant((prev) => prev.slice(0, -1))}
+              >
+                {DASH}
+              </button>
+            )}
           </div>
         </div>
         <div className="space-y-2">
           <ul className="border-y border-b-neutral-800 lg:border-t-neutral-800 space-y-2 py-2">
-            {Array.from({ length: prCount }).map((_, i) => (
+            {product.map((_, i) => (
               <li key={i}>
                 <input
-                  className="input w-full input-disabled"
+                  className="input w-full"
                   placeholder={lang === "中" ? "产出物" : "Product"}
+                  onChange={(e) =>
+                    setProduct((prev) => {
+                      prev[i] = e.target.value;
+                      return prev;
+                    })
+                  }
                 />
               </li>
             ))}
           </ul>
           <div className="flex gap-2 me-2">
-            <button
-              type="button"
-              className="btn btn-sm"
-              onClick={() => prCount <= 4 && setPrCount(prCount + 1)}
-            >
-              {PLUS}
-            </button>
-            <button
-              type="button"
-              className="btn btn-sm"
-              onClick={() => prCount >= 2 && setPrCount(prCount - 1)}
-            >
-              {DASH}
-            </button>
+            {product.length <= 2 && (
+              <button
+                type="button"
+                className="btn btn-sm"
+                onClick={() => setProduct((prev) => [...prev, ""])}
+              >
+                {PLUS}
+              </button>
+            )}
+            {product.length >= 2 && (
+              <button
+                type="button"
+                className="btn btn-sm"
+                onClick={() => setProduct((prev) => prev.slice(0, -1))}
+              >
+                {DASH}
+              </button>
+            )}
           </div>
         </div>
       </form>
-      <p className="p-2">{lang === "中" ? "前端施工中..." : "Frontend under development..."}</p>
+      <table className="table md:absolute md:bottom-0">
+        <thead>
+          <tr>
+            <th>{lang === "中" ? "类别" : "Category"}</th>
+            <th>{lang === "中" ? "参数" : "Data"}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>{lang === "中" ? "反应物" : "Reactant"}</th>
+            <td>{reResult}</td>
+          </tr>
+          <tr>
+            <th>{lang === "中" ? "产出物" : "Product"}</th>
+            <td>{prResult}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Note = ({ lang }: Props) => {
+  return (
+    <div className="md:row-span-2 py-2 flex flex-col items-center justify-center border-y border-neutral-800">
+      <h3 className="text-lg font-bold text-elypink">
+        {lang === "中" ? "写在最后" : "EXTRA NOTES"}
+      </h3>
+      <p className="max-w-lg">
+        {lang === "中"
+          ? "输入分子之后，后端会自动尝试计算，不过项目是高中的时候为了11年级化学题写的，所以给出的答案不一定是最好的，同时，这也是我的第一个大项目，所以整体也写的也挺烂的。"
+          : "You need enter the molecules, and the backend will try to calculate them automatically. However, since this project was written in high school for high school level chemistry, typically grade 11, it might not give you the best answer. Plus, this was my first ever major project, meaning it was not well written anyways."}
+      </p>
     </div>
   );
 };
@@ -249,7 +339,7 @@ const PLUS = (
     viewBox="0 0 16 16"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
     />
   </svg>
@@ -264,7 +354,7 @@ const DASH = (
     viewBox="0 0 16 16"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"
     />
   </svg>
